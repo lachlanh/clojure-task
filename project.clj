63e0b3724afe2e2cd44532b0cad061e144e6c878
lachlanh <lachlan@hesp.es>
2022-08-09 13:26:41 +0200

    update version

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Library to transform an integer to natural language version."
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :plugins [[lein-cloverage "1.2.2"]]
   :repl-options {:init-ns clojure-task.core})
